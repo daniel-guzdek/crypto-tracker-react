3c40e692502874ba1571fd1ef95c66f1d378c730
Daniel Guzdek <daniel.guzdek.86@gmail.com>
2021-06-27 21:03:26 +0200

    Added units

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -7,10 +7,10 @@ function Coin({id, image, name, symbol, market_cap, current_price, total_volume,
       <div className="coin__image claster"><img src={image} alt={symbol} className="icon"/></div>
       <div className="coin__name claster">{name}</div>
       <div className="coin__symbol claster">{symbol}</div>
-      <div className="coin__market_cap claster">{market_cap}</div>
-      <div className="coin__total_volume claster">{total_volume}</div>
-      {price_change_percentage_24h > 0 ? <div className="coin__price_change_24h claster growth">{price_change_percentage_24h}</div> : <div className="coin__price_change_24h claster drop">{price_change_percentage_24h}</div>}
-      <div className="coin__current_price claster">{current_price}</div>
+      <div className="coin__market_cap claster">{market_cap}<span className="unit">USD</span></div>
+      <div className="coin__total_volume claster">{total_volume}<span className="unit">USD</span></div>
+      {price_change_percentage_24h > 0 ? <div className="coin__price_change_24h claster growth">{price_change_percentage_24h}<span className="persentage_char">%</span></div> : <div className="coin__price_change_24h claster drop">{price_change_percentage_24h}<span className="persentage_char">%</span></div>}
+      <div className="coin__current_price claster">{current_price}<span className="unit">USD</span></div>
     </div>
   )
 }
